db7832e753e942605c07b211d329c248ae25b0f0
susanasuav <susanasuav@gmail.com>
2017-10-24 10:15:57 +0200

    this is a new change in my code

--- src/javaappnewgithub/JavaAppNewGitHub.java
+++ src/javaappnewgithub/JavaAppNewGitHub.java
@@ -12,6 +12,8 @@ public class JavaAppNewGitHub {
     public static void main(String[] args) {
         System.out.println("Hola per primer cop, 10:08");
         System.out.println("This is a modification at 10:13 ");
+        System.out.println("Aquí ho fem al recvés 10:15");
+        
     }
     
 }
