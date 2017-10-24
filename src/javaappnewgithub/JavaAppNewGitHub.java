3a2491c545a9c84af1233db553aee3f1f6924fa0
susana <susana@susana>
2017-10-24 10:09:56 +0200

    It's an example for DAW BIO 2 class

---
+++ src/javaappnewgithub/JavaAppNewGitHub.java
@@ -0,0 +1,16 @@
+package javaappnewgithub;
+
+/**
+ *This is an example for DAW BIO 2 class 
+ * @author susana
+ */
+public class JavaAppNewGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola per primer cop, 10:08");
+    }
+    
+}
